43f2c99408e8a871cc75a5b5bf935ed89b0c2897
gil <ryogo5.0304@gmail.com>
2026-06-24 09:31:46 +0900

    Initial Commit

---
+++ CountdownPoster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountdownPoster
+//
+//  Created by 落合遼梧 on 2026/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountdownPoster/CountdownPosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownPosterApp.swift
+//  CountdownPoster
+//
+//  Created by 落合遼梧 on 2026/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CountdownPosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
